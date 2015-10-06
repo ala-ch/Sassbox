26f8bc62dd055fa70eebb52b2d95413023a2a59a
Rene Keller <rek@ala.ch>
2015-10-06 09:38:36 +0200

    fixed timestamp

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
                   '// <%= pkg.repository.url %>\n' +
                   '// (c) 2015, <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> <<%= pkg.author.url %>>\n' + 
                   '// <%= pkg.license %>\n' + 
-                  '// <%= grunt.template.today("dd.mm.yyyy HH:mm") %>\n\n'
+                  '// <%= grunt.template.today("dd.mm.yyyy HH:MM") %>\n\n'
         },
         src: ['<%= sassboxPath %>/{,*/}_*.scss'],
         dest: '<%= sassboxFile %>'
